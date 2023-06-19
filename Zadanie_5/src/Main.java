dd7633f9494e80aa0bb89aa2e5430c8f4e63e05f
JakubMakulewski <jim.makulewsky@gmail.com>
2023-06-19 12:22:14 +0200

    Zadanie_4 source code added

--- Zadanie_5/src/Main.java
+++ Zadanie_5/src/Main.java
@@ -1,5 +1,373 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.regex.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static abstract class Figure{
+        String label;
+
+        String getLabel(){return label;}
+        void setLabel(String label){
+            this.label = label;
+        }
+        void move(double dx, double dy){}
+
+        double getArea(){return 0;}
+    }
+
+    public static class Point extends Figure{
+        private double x;
+        private double y;
+
+        public Point() {
+            this.x = 0.00;
+            this.y = 0.00;
+            this.label = "no label set";
+        }
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+            this.label = "no label set";
+        }
+
+        public Point(double x, double y, String label){
+            this.x = x;
+            this.y = y;
+            this.label = label;
+        }
+
+        public void move(double dx, double dy) {
+            x+=dx; y+=dy;
+        }
+
+        @Override
+        public String toString(){
+            if (label == "no label set")
+                return "Punkt> x:" + x + ", y:" + y + "\n";
+            else
+                return "Punkt> x:" + x + ", y:" + y + " - " + label + " -\n";
+        }
+
+        public double getArea(){
+            return 0;
+        }
+    }
+
+    public static class Section extends Figure{
+        private Point pointA;
+        private Point pointB;
+
+        public Section() {
+            this.pointA.x = 0;
+            this.pointA.y = 0;
+            this.pointB.x = 0;
+            this.pointB.y = 0;
+            this.label = "no label set";
+        }
+
+        public Section(Point pointA, Point pointB) {
+            this.pointA = pointA;
+            this.pointB = pointB;
+            this.label = "no label set";
+        }
+
+        public Section(Point pointA, Point pointB, String label) {
+            this.pointA = pointA;
+            this.pointB = pointB;
+            this.label = label;
+        }
+
+        public void move(double dx, double dy){
+            pointA.move(dx, dy);
+            pointB.move(dx, dy);
+        }
+
+        @Override
+        public String toString(){
+            if(label == "no label set")
+                return "Odcinek> { 1: "+pointA+" 2: "+pointB+" }\n";
+            else
+                return "Odcinek> { 1: "+ pointA +" 2: "+pointB+" } - " + label + " -\n";
+        }
+        public double getArea(){
+            return 0;
+        }
+    }
+
+    public static abstract class absCircle extends Figure{
+        abstract public void move(double dx, double dy);
+
+        abstract public String toString();
+
+        abstract public double getArea();
+    }
+
+    public static class Circle extends absCircle{
+        private Point srodek;
+        private double promien;
+
+        public Circle() {
+            this.srodek.x = 0;
+            this.srodek.y = 0;
+            this.promien = 0;
+            this.label = "no label set";
+        }
+
+        public Circle(Point srodek, double promien) {
+            this.srodek = srodek;
+            this.promien = promien;
+            this.label = "no label set";
+        }
+
+        public Circle(Point srodek, double promien, String label){
+            this.srodek = srodek;
+            this.promien = promien;
+            this.label = label;
+        }
+
+        public void move(double dx, double dy) {
+            srodek.move(dx, dy);
+        }
+
+        @Override
+        public String toString() {
+            if(label == "no label set")
+                return "Koło> { Środek = " + srodek.toString() + " Promień = " + this.promien + " }\n";
+            else
+                return "Koło> { Środek = " + srodek.toString() + " Promień = " + this.promien + " } - " + label + " -\n";
+        }
+
+
+        public double getArea(){
+            return this.promien*this.promien*3.1415;
+        }
+    }
+    public static abstract class Picture extends Figure{
+
+        public ArrayList<Figure> elements;
+
+        public Picture(){
+            this.elements = new ArrayList<>();
+        }
+
+        abstract boolean addElement(Figure element);
+
+        @Override
+        public void move(double dx, double dy) {
+            for (Figure shape : elements) {
+                shape.move(dx, dy);
+            }
+        }
+
+        public double getArea(){
+            double area = 0.00;
+            for (Figure s : elements){
+                area += s.getArea();
+            }
+            return area;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Obraz:\n");
+
+            sb.append("Elementy:\n");
+            for (Figure s : elements) {
+                sb.append(s.toString());
+            }
+            return sb.toString();
+        }
+    }
+    public static class UniquePicture extends Picture{
+
+        @Override
+        public boolean addElement(Figure element) {
+            for (Figure s : elements) {
+                if (s.getLabel().equals(element.getLabel())) {
+                    System.out.println("Błąd. Ta etykieta już istnieje");
+                    return false;
+                }
+            }
+            elements.add(element);
+            return true;
+        }
+
+    }
+    public static class StandarizedPicture extends Picture{
+        public boolean addElement(Figure element) {
+            String tag = element.getLabel();
+            Pattern labelPattern = Pattern.compile("^[A-Z][A-Z0-9]*$");         //sprawdzić poprawność kompilacji Pattern
+            Matcher labelMatch = labelPattern.matcher(tag);
+            if (labelMatch.matches()) {
+                elements.add(element);
+                return true;
+            }
+            System.out.println(tag+" zawiera niedozwolone znaki");
+            return false;
+        }
+    }
+
+
+
+    public static void main(String[]args){
+
+        Scanner scanner = new Scanner(System.in);
+        String opcja;
+        int wybierzObraz;
+
+        UniquePicture picture = new UniquePicture();
+        StandarizedPicture StdPicture = new StandarizedPicture();
+
+        do {
+            opcja = "";
+            wybierzObraz = 0;
+
+            System.out.println("1. Dodaj do obrazu\n2. Wyświetl Obraz\n3. Przesuń Obraz\n4. Wyświetl Sumę Pól\nw Wyjdź\nWybierz>");
+            opcja = scanner.nextLine();
+            switch (opcja) {
+                case "1":
+                    System.out.println("Do którego obrazu chcesz dodać?\n1 UniquePicture\n2 StandarizedPicture\nWybierz>");
+                    wybierzObraz=scanner.nextInt();
+                    scanner.nextLine();
+                    if(wybierzObraz == 1 || wybierzObraz == 2){
+                        opcja = "";
+                        System.out.println("1 Punkt\n2 Odcinek\n3 Okrąg\nWybierz>");
+                        opcja = scanner.nextLine();
+                        switch (opcja) {
+
+                            case "1":
+                                System.out.println("Wprowadź koordynat x:");
+                                double x = scanner.nextDouble();
+                                System.out.println("Wprowadź koordynat y:");
+                                double y = scanner.nextDouble();
+                                System.out.println("Dodaj label (opcjonalne, ENTER by pominąć)");
+                                scanner.nextLine();
+                                String labelPoint = scanner.nextLine();
+
+                                Point p = new Point(x, y, labelPoint);
+                                if(wybierzObraz==1){
+                                    picture.addElement(p);
+                                }else if(wybierzObraz==2){
+                                    StdPicture.addElement(p);
+                                }
+                                wybierzObraz=0;
+                                break;
+
+                            case "2":
+                                System.out.println("Wprowadź koordynaty początku nowego odcinka:");
+                                System.out.print("Wprowadź x: ");
+                                double xA = scanner.nextDouble();
+                                System.out.print("Wprowadź y: ");
+                                double yA = scanner.nextDouble();
+
+                                System.out.println("Wprowadź koordynaty końca nowego odcinka:");
+                                System.out.print("Wprowadź x: ");
+                                double xB = scanner.nextDouble();
+                                System.out.print("Wprowadź y: ");
+                                double yB = scanner.nextDouble();
+                                System.out.println("Dodaj label (opcjonalne, ENTER by pominąć)");
+                                scanner.nextLine();
+                                String labelSection = scanner.nextLine();
+                                Point p1 = new Point(xA, yA);
+                                Point p2 = new Point(xB, yB);
+
+                                Section se = new Section(p1, p2, labelSection);
+                                if(wybierzObraz==1){
+                                    picture.addElement(se);
+                                }else if(wybierzObraz==2){
+                                    StdPicture.addElement(se);
+                                }
+                                wybierzObraz=0;
+                                System.out.println("Nowy odcinek stworzony: " + se.toString());
+                                scanner.nextLine();
+                                break;
+
+                            case "3":
+                                System.out.println("Wprowadź koordynaty środka nowego koła:");
+                                System.out.print("Wprowadź x: ");
+                                double xKolo = scanner.nextDouble();
+                                System.out.print("Wprowadź y: ");
+                                double yKolo = scanner.nextDouble();
+                                System.out.print("Wprowadź promień: ");
+                                double promien = scanner.nextDouble();
+                                System.out.println("Dodaj label (opcjonalne, ENTER by pominąć)");
+                                scanner.nextLine();
+                                String labelCircle = scanner.nextLine();
+
+                                Point k = new Point(xKolo, yKolo);
+                                Circle c = new Circle(k, promien, labelCircle);
+                                if(wybierzObraz==1){
+                                    picture.addElement(c);
+                                }else if(wybierzObraz==2){
+                                    StdPicture.addElement(c);
+                                }
+                                wybierzObraz=0;
+                                System.out.println("Nowe koło stworzone: " + c.toString());
+                                scanner.nextLine();
+                                break;
+                        }
+                    }
+                    else {
+                        System.out.println("Błąd. Nie ma takiego obrazu");
+                    }
+                    break;
+                case "2":
+                    System.out.println("Który obraz chcesz wyświetlić?\n1 UniquePicture\n2 StandarizedPicture\nWybierz>");
+                    wybierzObraz = scanner.nextInt();
+                    scanner.nextLine();
+                    if(wybierzObraz == 1 || wybierzObraz ==2){
+                        if(wybierzObraz==1){
+                            System.out.println(picture.toString());
+                        }else if(wybierzObraz==2){
+                            System.out.println(StdPicture.toString());
+                        }
+                        wybierzObraz=0;
+                    }else{
+                        System.out.println("Błąd. Nie ma takiego obrazu");
+                    }
+                    break;
+                case "3":
+                    System.out.println("Który obraz chcesz przesunąć?\n1 UniquePicture\n2 StandarizedPicture\nWybierz>");
+                    wybierzObraz = scanner.nextInt();
+                    scanner.nextLine();
+                    if(wybierzObraz == 1 || wybierzObraz == 2){
+                        System.out.print("Wprowadź dx: ");
+                        double dx = scanner.nextDouble();
+                        System.out.print("Wprowadź dy: ");
+                        double dy = scanner.nextDouble();
+                        if(wybierzObraz==1){
+                            picture.move(dx, dy);
+                        }else if(wybierzObraz==2){
+                            StdPicture.move(dx, dy);
+                        }
+                        wybierzObraz=0;
+                    }else{
+                        System.out.println("Błąd. Nie ma takiego obrazu");
+                    }
+                    break;
+                case "4":
+                    double area=0;
+                    System.out.println("Sumę pól którego obrazu chcesz uzyskać?\n1 UniquePicture\n2 StandarizedPicture\nWybierz>");
+                    wybierzObraz = scanner.nextInt();
+                    scanner.nextLine();
+                    if(wybierzObraz == 1 || wybierzObraz ==2){
+                        if(wybierzObraz==1){
+                            area = picture.getArea();
+                        }else if(wybierzObraz==2){
+                            area = StdPicture.getArea();
+                        }
+                        wybierzObraz=0;
+                        System.out.println("Suma pól: " + area);
+                    }else{
+                        System.out.println("Błąd. Nie ma takiego obrazu");
+                    }
+                    break;
+            }
+        } while (!"w".equals(opcja));
+        scanner.close();
     }
 }
